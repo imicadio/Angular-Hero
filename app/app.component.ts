f0e7e3ee7ca15171d100d2454d94f6b8eb00b753
imicadio <mrjesio@gmail.com>
2017-08-11 14:41:41 +0200

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <events-list></events-list>
+  <hr>
+  `
+})
+export class AppComponent {
+  
+}
